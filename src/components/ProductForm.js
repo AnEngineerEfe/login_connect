4a3a927ffa87f2d517f8fb8713f7c083051aa828
AnEngineerEfe <166179536+AnEngineerEfe@users.noreply.github.com>
2024-09-15 15:28:13 +0300

    Updated

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 const ProductForm = ({ onAddProduct, onSaveProduct, productToEdit }) => {
-  const initialValues = productToEdit || { name: '', price: '' };
-  
+  const initialValues = { name: '', price: '' };
+
+  // useEffect burada, productToEdit her değiştiğinde Formik değerlerini güncelleyecek
+  useEffect(() => {
+    if (productToEdit) {
+      setValues(productToEdit);
+    }
+  }, [productToEdit, setValues]);
+
   const validationSchema = Yup.object({
     name: Yup.string().required('Required'),
     price: Yup.number().required('Required'),
@@ -12,7 +19,7 @@ const ProductForm = ({ onAddProduct, onSaveProduct, productToEdit }) => {
 
   const handleSubmit = (values, { resetForm }) => {
     if (productToEdit) {
-      onSaveProduct(values);
+      onSaveProduct({ ...values, id: productToEdit.id }); // id'yi de ekleyerek güncelliyoruz
     } else {
       onAddProduct(values);
     }
@@ -21,23 +28,26 @@ const ProductForm = ({ onAddProduct, onSaveProduct, productToEdit }) => {
 
   return (
     <Formik
-      initialValues={initialValues}
+      initialValues={productToEdit || initialValues} // Başlangıç değerleri güncelleniyor
       validationSchema={validationSchema}
       onSubmit={handleSubmit}
+      enableReinitialize // Formun başlangıç değerlerinin dışarıdan güncellenebilmesini sağlıyor
     >
-      <Form>
-        <div>
-          <label htmlFor="name">Product Name</label>
-          <Field name="name" type="text" />
-          <ErrorMessage name="name" />
-        </div>
-        <div>
-          <label htmlFor="price">Price</label>
-          <Field name="price" type="number" />
-          <ErrorMessage name="price" />
-        </div>
-        <button type="submit">{productToEdit ? 'Save Changes' : 'Add Product'}</button>
-      </Form>
+      {({ setValues }) => (
+        <Form>
+          <div>
+            <label htmlFor="name">Product Name</label>
+            <Field name="name" type="text" />
+            <ErrorMessage name="name" />
+          </div>
+          <div>
+            <label htmlFor="price">Price</label>
+            <Field name="price" type="number" />
+            <ErrorMessage name="price" />
+          </div>
+          <button type="submit">{productToEdit ? 'Save Changes' : 'Add Product'}</button>
+        </Form>
+      )}
     </Formik>
   );
 };
